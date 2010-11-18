0949f8e3da21af8a78ba59e115c92b4be969db0f
Michel Hiemstra <info@michelhiemstra.nl>
2010-11-18 01:46:51 +0100

    Added json check

--- blocks_get.php
+++ blocks_get.php
@@ -1,10 +1,11 @@
-<?
+<?php
 
 /* establish the mysql connection */
 require_once 'mysql_connect.php';
 
 /* get all blocks and return in json */
 function getBlocks() {
+
 	$blocks = mysql_query("SELECT * FROM blocks ORDER BY order_id ASC");
 	
 	$columns = array();
@@ -14,6 +15,8 @@ function getBlocks() {
 		$columns[$block['column_id']][] = $block['block_id'];
 	}
 	
+	if (!function_exists('json_encode')) die('your server does not support json');
+		
 	return json_encode($columns);
 }
 
--- blocks_save.php
+++ blocks_save.php
@@ -1,4 +1,4 @@
-<?
+<?php
 
 /*
 	CREATE TABLE `blocks` (
